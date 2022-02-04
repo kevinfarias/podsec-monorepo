83f90832bcd61e90fcfb1555aacbdaac089efdc8
kevin farias <kevin.farias.dev@gmail.com>
2022-02-04 05:11:55 -0300

    limpando warnings eslint

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-magic-numbers */
 'use strict';
 
 const fs = require('fs');
--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import * as indexController from '../controllers/indexController';
 
+// eslint-disable-next-line new-cap
 const routes = express.Router();
 
 routes.get('/', indexController.homePage);
