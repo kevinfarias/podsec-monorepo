5ed7d2409c12684bbf64584bc55f94bd4df0a616
kevin farias <kevin.farias.dev@gmail.com>
2022-02-17 15:14:13 -0300

    initial

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,4 +6,20 @@ const routes = express.Router();
 
 routes.get('/', indexController.homePage);
 
+// NIVEIS DO REST:
+// 1. nao respeita corretamente os metodos nem mantem padroes, nivel 1 do rest
+// 2. buscamos: implementar as coisas seguindo os metodos HTTP, 
+// e as respostas de acordo, usar nomenclatura que façam sentidos
+// 3. rest auto navegavel:
+
+// GET /products -> traz registros
+// POST /products -> insere um novo registro
+// PUT /products/1 -> atualiza um registro
+// DELETE /products/1 -> deleta um registro
+// GET /producots/1 -> traz detalhes de um registro
+
+//// TESTES UNITÁRIOS: voce testa a menor parte possivel do teu código
+//// TESTES INTEGRAÇÃO: voce testa mais tecnologias
+//// TESTES END-TO-END: (futuro)
+
 export default routes;
