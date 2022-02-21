d37c91f120f5c009704d4dec9422e0cc6da00ec2
kevin farias <kevin.farias.dev@gmail.com>
2022-02-21 09:16:26 -0300

    correcoes seeds

--- backend/src/database/seeders/20220214195413-usuarios.js
+++ backend/src/database/seeders/20220214195413-usuarios.js
@@ -2,11 +2,34 @@
 
 module.exports = {
     async up(queryInterface, _Sequelize) {
-        await queryInterface.sequelize.query(`INSERT INTO usuarios 
-            (nomeCompleto,usuario,senha,email,situacao,dataCriacao,receberNewsletter,nivelAcesso) 
-            VALUES ('Administrador','administrador','tR@.d#ar','administrador@teinformatech.com', 
-            '1', '2022-02-17 22:12:09.361 +00:00' ,'0','1')
-            `);
+        const rawQuery = 'select pa.* from situacoes pa';
+        const typeSelect = { type: _Sequelize.QueryTypes.SELECT };
+        const rowsInDatabase = 
+            await queryInterface.sequelize.query(rawQuery, typeSelect);
+
+        if (rowsInDatabase.length === 0) {
+            require('dotenv/config');
+
+            const bcrypt = require('bcrypt');
+            const salt = process.env.BCRYPT_SALT;
+            const numberOfRounds = 15;
+
+            const password = `pietra${salt}`;
+            const hashedPassword = bcrypt.hashSync(password, numberOfRounds);
+
+            await queryInterface.bulkInsert('usuarios', [
+                {
+                    nomecompleto: 'Administrador',
+                    usuario: 'administrador',
+                    senha: hashedPassword,
+                    email: 'administrador@teinformatech.com',
+                    situacao: '1',
+                    datacriacao: '2022-02-17 22:12:09',
+                    recebernewsletter: '0',
+                    nivelacesso: 1
+                }
+            ]);
+        }
     },
 
     async down(queryInterface, _Sequelize) {
