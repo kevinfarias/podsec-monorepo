4bb08e97fb72ca3475b25dd138f1012f8d1c4616
Jean Marcos de Souza <jiamarcos@outlook.com.br>
2022-02-20 21:53:26 -0300

    Finalizado criação das seeders com hash de criptografia / Corrigido readme para atender novo instanciamento do docker

--- backend/src/database/seeders/20220214195345-niveis_acesso.js
+++ backend/src/database/seeders/20220214195345-niveis_acesso.js
@@ -2,12 +2,18 @@
 
 module.exports = {
     async up(queryInterface, _Sequelize) {
-        await queryInterface.bulkInsert('niveis_acesso',
-            [
-                { descricao: 'administrador' },
-                { descricao: 'criador-de-conteudo' },
-                { descricao: 'consumidor' }
-            ], {});
+        const rawQuery = 'select * from niveis_acesso';
+        const typeSelect = { type: _Sequelize.QueryTypes.SELECT };
+        const rowsInDatabase = await queryInterface.sequelize.query(rawQuery, typeSelect);
+
+        if (rowsInDatabase.length === 0) {
+            await queryInterface.bulkInsert('niveis_acesso',
+                [
+                    { descricao: 'administrador' },
+                    { descricao: 'criador-de-conteudo' },
+                    { descricao: 'consumidor' }
+                ], {});
+        }
     },
 
     async down(queryInterface, _Sequelize) {
--- backend/src/database/seeders/20220214195436-posts-situacoes.js
+++ backend/src/database/seeders/20220214195436-posts-situacoes.js
@@ -2,11 +2,17 @@
 
 module.exports = {
     async up(queryInterface, _Sequelize) {
-        await queryInterface.bulkInsert('situacoes', [
-            { descricao: 'criado' },
-            { descricao: 'publicado' },
-            { descricao: 'deletado' }
-        ], {});
+        const rawQuery = 'select * from situacoes';
+        const typeSelect = { type: _Sequelize.QueryTypes.SELECT };
+        const rowsInDatabase = await queryInterface.sequelize.query(rawQuery, typeSelect);
+
+        if (rowsInDatabase.length === 0) {
+            await queryInterface.bulkInsert('situacoes', [
+                { descricao: 'criado' },
+                { descricao: 'publicado' },
+                { descricao: 'deletado' }
+            ], {});
+        }
     },
 
     async down(queryInterface, _Sequelize) {
