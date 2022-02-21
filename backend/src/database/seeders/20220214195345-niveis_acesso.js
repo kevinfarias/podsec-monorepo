dbc1a01d0f4a9cd63ea6aa41341f711da99ed7d3
kevin farias <kevin.farias.dev@gmail.com>
2022-02-21 09:16:26 -0300

    correcoes seeds

--- backend/src/database/seeders/20220214195345-niveis_acesso.js
+++ backend/src/database/seeders/20220214195345-niveis_acesso.js
@@ -2,12 +2,19 @@
 
 module.exports = {
     async up(queryInterface, _Sequelize) {
-        await queryInterface.bulkInsert('niveis_acesso',
-            [
-                { descricao: 'administrador' },
-                { descricao: 'criador-de-conteudo' },
-                { descricao: 'consumidor' }
-            ], {});
+        const rawQuery = 'select pa.* from niveis_acesso pa';
+        const typeSelect = { type: _Sequelize.QueryTypes.SELECT };
+        const rowsInDatabase = 
+            await queryInterface.sequelize.query(rawQuery, typeSelect);
+
+        if (rowsInDatabase.length === 0) { 
+            await queryInterface.bulkInsert('niveis_acesso',
+                [
+                    { descricao: 'administrador' },
+                    { descricao: 'criador-de-conteudo' },
+                    { descricao: 'consumidor' }
+                ], {});
+        }
     },
 
     async down(queryInterface, _Sequelize) {
--- backend/src/database/seeders/20220214195436-posts-situacoes.js
+++ backend/src/database/seeders/20220214195436-posts-situacoes.js
@@ -2,11 +2,18 @@
 
 module.exports = {
     async up(queryInterface, _Sequelize) {
-        await queryInterface.bulkInsert('situacoes', [
-            { descricao: 'criado' },
-            { descricao: 'publicado' },
-            { descricao: 'deletado' }
-        ], {});
+        const rawQuery = 'select pa.* from situacoes pa';
+        const typeSelect = { type: _Sequelize.QueryTypes.SELECT };
+        const rowsInDatabase = 
+            await queryInterface.sequelize.query(rawQuery, typeSelect);
+
+        if (rowsInDatabase.length === 0) { 
+            await queryInterface.bulkInsert('situacoes', [
+                { descricao: 'criado' },
+                { descricao: 'publicado' },
+                { descricao: 'deletado' }
+            ], {});
+        }
     },
 
     async down(queryInterface, _Sequelize) {
